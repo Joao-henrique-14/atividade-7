957e3fd33912f8a0e912d8ac785dfe52a17a7052
Joao-henrique-14 <113247777+Joao-henrique-14@users.noreply.github.com>
2022-11-11 22:10:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let  ano= 2018;
+let nascimento= 1990;
+let idade=28
+idade= 2018 - 1990;
+
+
+let seu_nome, primeiro_nome, segundo_nome; 
+primeiro_nome='Agatha';
+segundo_nome='Christie';
+seu_nome=`Olá, seu nome é ${primeiro_nome} ${segundo_nome} e você tem ${idade} anos`
+alert(`Olá, seu nome é ${primeiro_nome} ${segundo_nome} e você tem ${idade} anos`)
